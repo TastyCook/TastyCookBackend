3f1d873b76006903cf1c48713b205cc2df3f594b
Andrew Fedin <andrii.fedin@nure.ua>
2023-06-11 23:28:47 +0300

    Hotfix

--- TastyCook.RecipesAPI/Models/RecipeModel.cs
+++ TastyCook.RecipesAPI/Models/RecipeModel.cs
@@ -10,7 +10,7 @@
         public bool? IsUserLiked { get; set; }
         public string? UserId { get; set; }
         public IEnumerable<string> Categories { get; set; }
-        public IEnumerable<string> Products { get; set; }
+        public IEnumerable<string> Products { get; set; } = Enumerable.Empty<string>();
         public string? ImageUrl { get; set; }
     }
 }
